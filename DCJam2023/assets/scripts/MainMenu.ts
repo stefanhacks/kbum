8a84a72023dcfde224437da39b8f223b6626f931
Thiago Ribeiro <thiagoa.ribeir@gmail.com>
2023-04-01 20:12:31 -0300

    feat: solo mode

--- DCJam2023/assets/scripts/MainMenu.ts
+++ DCJam2023/assets/scripts/MainMenu.ts
@@ -6,6 +6,7 @@
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
 import SoundController from "./SoundController";
+import { AIDifficulty, Config } from "./game/Configs";
 
 const {ccclass, property} = cc._decorator;
 
@@ -20,29 +21,61 @@ export default class MainMenu extends cc.Component {
     @property(cc.Button)
     private multiplayerButton: cc.Button = null;
 
-private async onPlayClicked() :Promise<void> {
-    SoundController.instance.play(SoundController.instance.menuSelect, false, 1);
-    this.fadeNodeOut(this.playButton.node);
-    this.fadeNodeIn(this.soloButton.node);
-    this.fadeNodeIn(this.multiplayerButton.node);
-}
+    @property(cc.Button)
+    private easyButton: cc.Button = null;
 
-private fadeNodeIn(node: cc.Node) :void {
-    new Promise<void>((resolve) => {
-        cc.tween(node).to(0.5, { opacity: 255 }, { easing: 'quartOut' }).call(resolve).start();
-    });
-}
+    @property(cc.Button)
+    private hardButton: cc.Button = null;
 
-private fadeNodeOut(node: cc.Node) :void {
-    new Promise<void>((resolve) => {
-        cc.tween(node).to(0.5, { opacity: 0 }, { easing: 'quartOut' }).call(resolve).start();
-    });
-}
+    @property(cc.Button)
+    private youWillDieButton: cc.Button = null;
 
-private changeScene(): void {
-    SoundController.instance.playBackground(SoundController.instance.bgWind);
-    SoundController.instance.play(SoundController.instance.saloonDoorSqueak, false, 0.5);
-    SoundController.instance.playEffect(SoundController.instance.promptPrepare);
-    cc.director.loadScene('Game');
-}
+    private async onPlayClicked() :Promise<void> {
+        SoundController.instance.play(SoundController.instance.menuSelect, false, 1);
+        this.fadeNodeOut(this.playButton.node);
+        this.fadeNodeIn(this.soloButton.node);
+        this.fadeNodeIn(this.multiplayerButton.node);
+    }
+
+    private async onSoloClicked() :Promise<void> {
+        this.fadeNodeOut(this.soloButton.node);
+        this.fadeNodeOut(this.multiplayerButton.node);
+        this.fadeNodeIn(this.easyButton.node);
+        this.fadeNodeIn(this.hardButton.node);
+        this.fadeNodeIn(this.youWillDieButton.node);
+    }
+
+    private async onEasyClicked() :Promise<void> {
+        this.changeScene(AIDifficulty.Easy);
+    }
+
+    private async onHardClicked() :Promise<void> {
+        this.changeScene(AIDifficulty.Hard);
+    }
+
+    private async onYouWillDieClicked() :Promise<void> {
+        this.changeScene(AIDifficulty.YouWillDie);
+    }
+
+    private fadeNodeIn(node: cc.Node) :void {
+        node.active = true;
+        new Promise<void>((resolve) => {
+            cc.tween(node).to(0.5, { opacity: 255 }, { easing: 'quartOut' }).call(resolve).start();
+        });
+    }
+
+    private fadeNodeOut(node: cc.Node) :void {
+        node.active = false
+        new Promise<void>((resolve) => {
+            cc.tween(node).to(0.5, { opacity: 0 }, { easing: 'quartOut' }).call(resolve).start();
+        });
+    }
+
+    private changeScene(difficulty: AIDifficulty): void {
+        SoundController.instance.playBackground(SoundController.instance.bgWind);
+        SoundController.instance.play(SoundController.instance.saloonDoorSqueak, false, 0.5);
+        SoundController.instance.playEffect(SoundController.instance.promptPrepare);
+        Config.instance.difficulty = difficulty;
+        cc.director.loadScene('Game');
+    }
 }
